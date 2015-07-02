2d61ce211757fad0ee8a1f90279e0d5de41eb0e2
pawankumarpathak <pawanpathak031991@gmail.com>
2015-07-02 07:15:17 +0530

    thrid

--- Person2.sql
+++ Person2.sql
@@ -3,17 +3,7 @@ CREATE TABLE Person
 FirstName VARCHAR(100),
 LastName VARCHAR(100),
 Gender VARCHAR(100) ); 
-INSERT INTO Person VALUES (1,'Ram','Gupta','Male');
-INSERT INTO Person VALUES (2,'Shayam','Gupta','Male');
-INSERT INTO Person VALUES (3,'Priya','Gupta','Female');
-INSERT INTO Person VALUES (4,'Sonali','Sharma','Female');
-INSERT INTO Person VALUES (5,'Nancy','Joshi','Female');
-INSERT INTO Person VALUES (6,'Anita','Advani','Female'); 
-INSERT INTO Person VALUES (7,'Anita','Joshi','Female'); 
-INSERT INTO Person VALUES (8,'Ram','Joshi','Male');
-INSERT INTO Person VALUES (9,'Priya','Panday','Female');
-INSERT INTO Person VALUES (10,'Rahim','Aneja','Male'); 
-INSERT INTO Person VALUES (11,'Rahim','Advani','Male');
+
 Select * from  Person where ID <=5  
 Select * from Person where FirstName='Ram' and LastName='Gupta'or FirstName='Rahim'  
 Select * from  Person where Gender='male'
